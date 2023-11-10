fc5d88f4e871f9c222a2ce11570f557411591c49
goodays01 <princessudeh230@gmail.com>
2023-11-10 13:17:08 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the first entry
+ *
+ * Return: value should be o
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

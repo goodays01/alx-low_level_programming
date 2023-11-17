4744d140ac24d2083b5d7b0ddd1a2ba7621950ae
goodays01 <princessudeh230@gmail.com>
2023-11-16 19:22:51 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: if successful is 0
+ */
+
+int main(void)
+
+{
+	char p;
+
+	for (p = 'a' ; p <= 'z' ; p++)
+		putchar(p);
+	putchar('\n');
+		return (0);
+}
